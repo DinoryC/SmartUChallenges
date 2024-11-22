f51ba3893ed0f5591aabcee5a248bd9b1167c5a6
Theo <dinorychang@gmail.com>
2024-11-22 14:45:48 +1100

    include react library

---
+++ src/MyComponent.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const MyComponent = () => (
+  <div>
+     <h1>My Favourite Foods</h1>
+     <ul>
+       <li>Bacon</li>
+       <li>Jamon</li>
+       <li>Noodles</li>
+     </ul>
+   </div>
+);
+
+export default MyComponent;
+
+// import ReactDOM from "react-dom";
+
+// ReactDOM.render(
+//   <div>
+//     <h1>My Favourite Foods</h1>
+//     <ul>
+//       <li>Bacon</li>
+//       <li>Jamon</li>
+//       <li>Noodles</li>
+//     </ul>
+//   </div>,
+//   document.getElementById("root")
+// );
---
+++ webpack.config.cjs
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/indexTest.jsx', // Entry point of your React code
+  output: {
+    path: path.resolve(__dirname, 'public/react_javascripts'),
+    filename: 'bundle.js', // Output bundle
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/, // Transpile both .js and .jsx files
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'], // Resolve these extensions
+  },
+};
