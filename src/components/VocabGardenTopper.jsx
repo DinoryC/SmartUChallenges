ef411dd35a4148c57dcd3038d29e6bdc680ca498
Theo <dinorychang@gmail.com>
2024-12-16 19:56:51 +1100

    adjust topper content and UI

--- src/components/VocabGardenTopper.jsx
+++ src/components/VocabGardenTopper.jsx
@@ -16,15 +16,20 @@ const VocabGardenTopper = (props) => {
 
   return (
     <div className="vocabGardenTopper">
-      <h1 className="headingStyle">Welcome to Vocabulary Garden</h1>
-
+      <h1 className="headingStyle">
+        <span role="img" aria-label="leaf sprout" className="emoji">🌱</span>
+        <span role="img" aria-label="books" className="emoji">📚</span>
+        Welcome to Vocabulary Garden
+        <span role="img" aria-label="pencil for writing" className="emoji">✏️</span>
+        <span role="img" aria-label="butterfly" className="emoji">🦋</span>
+      </h1>
 
       <div className="position-relative w-100 mt-3" style={{ height: "auto" }}>
         {/* Left Div: Start at 10%, Width 24% */}
         <div
           className="position-absolute greetingUser text-wrap"
           style={{
-            left: "9%",
+            left: "5%",
             width: "25%",
           }}
         >
@@ -33,26 +38,10 @@ const VocabGardenTopper = (props) => {
           )}
         </div>
 
-        {/* Middle Div: Centered, Width 20% */}
-        <div
-          className="position-absolute decorationStyle text-center"
-          style={{
-            left: "50%",
-            width: "24%",
-            transform: "translateX(-50%)",
-          }}
-        >
-          <span role="img" aria-label="leaf sprout" className="emoji">🌱</span>
-          <span role="img" aria-label="books" className="emoji">📚</span>
-          <span role="img" aria-label="pencil for writing" className="emoji">✏️</span>
-          <span role="img" aria-label="butterfly" className="emoji">🦋</span>
-        </div>
-
-        {/* Right Div: Ends at 10% from the Right */}
         <div
           className="position-absolute logOutButton text-end"
           style={{
-            right: "3%",
+            right: "5%",
           }}
         >
           {props.authState.isAuthenticated && (
